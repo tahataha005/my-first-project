1a84d35d7e073286cbba07d95b3fb062914dc0ce
Ahmad Saaty <ahmad.o.saaty@gmail.com>
2025-02-09 16:00:48 +0200

    added script

---
+++ script.py
@@ -0,0 +1,10 @@
+#data types in python 
+print("Hello Ahmad!") #string
+print(100) #integer
+print(1.5) #float
+print(True) #boolean
+
+name = input("Please input name: ")
+year = input("Please input birthyear: ")
+
+print("Hello " + name + " you are born on " + year)
